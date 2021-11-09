8dbeb66e81cb456eeefb62c6c5a1d7c1814474dd
Michelle <mchristine.stone91@gmail.com>
2021-11-09 01:47:33 -0500

    prevent empty fields and reset the form

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,6 +8,14 @@ var taskFormHandler = function (event) {
     "select[name = 'task-type']"
   ).value;
 
+  //check if input values are empty strings
+  if (!taskNameInput || !taskTypeInput) {
+    alert("You need to fill out the task form!");
+    return false;
+  }
+
+  formEl.reset();
+
   //package up data as an object
   var taskDataObj = {
     name: taskNameInput,
