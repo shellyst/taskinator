02c8b9a47e6604b8837203b9ca67ceb25d0800ae
Michelle <mchristine.stone91@gmail.com>
2021-11-08 23:25:15 -0500

    capture form fields

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,9 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 var createTaskHandler = function (event) {
   event.preventDefault();
 
+  var taskNameInput = document.querySelector("input[name = 'task-name']");
+  console.dir(taskNameInput);
+
   var listItemEl = document.createElement("li");
   listItemEl.className = "task-item";
   listItemEl.textContent = "This is a new task";
